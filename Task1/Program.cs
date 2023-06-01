7d026968c8429216013abb37635e38f8c90e7d33
Boris <89241300089@mail.ru>
2023-06-01 10:42:40 +0900

    Added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+class Program {
+    public static void Main (string[] args) {
+        Console.Write("Введите первое число: ");
+        double firstNumber = GetDoubleNumber();
+        Console.Write("Введите второе число: ");
+        int secondNumber = GetIntNumber();
+        double result = 1;
+        for (int i = 1; i <= secondNumber; i++)
+        {
+            result *= firstNumber;
+        }
+  
+        Console.WriteLine($"Степень числа {firstNumber} с натуральным показателем {secondNumber} равна {result}");
+     }
+
+     static int GetIntNumber() {
+        String stringNumber = Console.ReadLine() ?? "Значение Null";
+        int number;
+        bool isNumber = int.TryParse(stringNumber, out number);
+        return number;
+     }
+
+     static double GetDoubleNumber() {
+        String stringNumber = Console.ReadLine() ?? "Значение Null";
+        double number;
+        bool isNumber = Double.TryParse(stringNumber, out number);
+        return number;
+     }
+}
